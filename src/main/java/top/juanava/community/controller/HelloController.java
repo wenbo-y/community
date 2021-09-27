382164af16b173ea63166c5382ef9171c3ed56d9
unknown <yuanwb.ei@gmail.com>
2021-09-28 00:26:20 +0800

    init commit

---
+++ src/main/java/top/juanava/community/controller/HelloController.java
@@ -0,0 +1,17 @@
+package top.juanava.community.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@Controller
+public class HelloController {
+    @GetMapping("/hello")
+    public String hello(@RequestParam(value = "name") String name,
+                        Model model){
+                            model.addAttribute("name", name);
+                            return "hello";
+    }
+}
